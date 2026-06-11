506e076c2c47fddbe23031d71d5c1fb86801a265
PietroSantos280 <santospietro0910@gmail.com>
2026-06-11 14:44:21 -0300

    finalização da aula09

--- aula09_Funcoes/exercicios/exercicio05.php
+++ aula09_Funcoes/exercicios/exercicio05.php
@@ -20,7 +20,11 @@ pesquisar sobre a função nativa array_sum() do PHP. -->
 <?php
     $temperaturas = [22.5, 24.0, 25.5, 23.0];
     function calcularMediaTemperatura($listaDeTemperaturas){
-        soma =  array_sum()   
-        media = 
+        $soma = array_sum($listaDeTemperaturas);
+        $n = count($listaDeTemperaturas);   
+        $media = $soma / $n;
+        return $media;
     }
+    $resultado = calcularMediaTemperatura($temperaturas);
+    echo "A média da temperaturas registradas no Arduino é de: $resultado";
 ?>
